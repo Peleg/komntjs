7901be14b83d72cf1b0c3b15d7e573d2afceae9f
Peleg Rosenthal <peleg@teacherspayteachers.com>
2015-11-27 13:40:19 -0500

    adding webpack

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 # Builds dist/komnt.js and dist/komnt.css
 all:
-	webpack src/*.js dist/komnt.js
+	npm install
+	./node_modules/webpack/bin/webpack.js
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+var path    = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/Komnt.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'komnt.js',
+    publicPath: '/static/'
+  }
+};
